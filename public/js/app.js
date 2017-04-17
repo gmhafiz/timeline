60c81e784ee758feaaaa460b662216c7ebc22873
Hafiz Shafruddin <gmhafiz@gmail.com>
2017-04-17 12:21:12 +1000

    fix timeline viz div overflowing

--- public/js/app.js
+++ public/js/app.js
@@ -16,40 +16,42 @@ var items = new vis.DataSet(
 //     [{"id":4,"content":"t1","start":"2017-04-16","end":"2017-04-19","type":"","title":"","group":"","subgroup":"","style":"","createdAt":"2017-04-17T02:14:44.166869942+10:00","updatedAt":"2017-04-17T02:14:44.166869942+10:00"},{"id":5,"content":"t1","start":"2017-04-16","end":"2017-04-19","type":"","title":"","group":"","subgroup":"","style":"","createdAt":"2017-04-17T02:14:46.095823387+10:00","updatedAt":"2017-04-17T02:14:46.095823387+10:00"},{"id":6,"content":"t1sfaf","start":"2017-04-16","end":"2017-04-19","type":"","title":"","group":"","subgroup":"","style":"","createdAt":"2017-04-17T02:15:17.604591386+10:00","updatedAt":"2017-04-17T02:15:17.604591386+10:00"},{"id":7,"content":"t1","start":"2017-04-16","end":"2017-04-19","type":"","title":"","group":"","subgroup":"","style":"","createdAt":"2017-04-17T02:18:58.109212081+10:00","updatedAt":"2017-04-17T02:18:58.109212081+10:00"},{"id":8,"content":"t1","start":"2017-04-16","end":"2017-04-19","type":"","title":"","group":"","subgroup":"","style":"","createdAt":"2017-04-17T02:23:13.554703373+10:00","updatedAt":"2017-04-17T02:23:13.554703373+10:00"},{"id":9,"content":"t1","start":"2017-04-17","end":"2017-04-20","type":"","title":"","group":"","subgroup":"","style":"","createdAt":"2017-04-17T02:23:22.393150852+10:00","updatedAt":"2017-04-17T02:23:22.393150852+10:00"}]
 );
 
+
+
 // Configuration for the Timeline
 var options = {
     width: '100%',
     height: '500px',
-    editable: true
+    selectable: true,
+    editable: {
+        add: true,
+        updateTime: true,
+        updateGroup: true,
+        remove: true,
+        overrideItems: false
+    },
+
+    onRemove: function(item, callback)  {
+        // prettyConfirm('Remove item', 'Do you really want to remove item ' + item.content + '?', function (ok) {
+        var ok = true;
+            if (ok) {
+                callback(item);
+                // todo: update db
+                console.log("removed item id: " + item.id);
+                deleteItemDB(item.id);
+            } else {
+                callback (null);
+            }
+        // });
+    }
 };
 
 // Create a Timeline
 var timeline = new vis.Timeline(container, items, options);
 
 
-function updateVis(data) {
-    var container = document.getElementById('visualization');
-    var items = new vis.DataSet(data);
-    var options = {
-        width: '100%',
-        height: '500px',
-        editable: true
-    };
-    // var timeline = new vis.Timeline(container, items, options);
-    timeline.redraw();
-}
 
 
-function ConvertFormToJSON(form){
-    var array = jQuery(form).serializeArray();
-    var json = {};
-
-    jQuery.each(array, function() {
-        json[this.name] = this.value || '';
-    });
-
-    return JSON.stringify(json);
-}
 
 jQuery(document).on('ready', function () {
     jQuery('form#addEvent').bind('submit', function (ev) {
@@ -104,3 +106,63 @@ jQuery(document).on('ready', function () {
         })
     });
 });
+
+function ConvertFormToJSON(form){
+    var array = jQuery(form).serializeArray();
+    var json = {};
+
+    jQuery.each(array, function() {
+        json[this.name] = this.value || '';
+    });
+
+    return JSON.stringify(json);
+}
+
+function deleteItemDB(id) {
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:8080/api/event/" + id,
+        data: null,
+        dataType: "json",
+        contentType: "application/json",
+        success: function(data) {
+            console.log("Removed item :" + id);
+        },
+        error: function (data) {
+            console.log("Unsuccessful removal of item " + id);
+            console.log(data);
+        }
+    })
+}
+
+items.on('*', function (event, properties) {
+    logEvent(event, properties);
+});
+
+function logEvent(event, properties) {
+    var log = document.getElementById('log');
+    var msg = document.createElement('div');
+    msg.innerHTML = 'event=' + JSON.stringify(event) + ', ' +
+        'properties=' + JSON.stringify(properties);
+    log.firstChild ? log.insertBefore(msg, log.firstChild) : log.appendChild(msg);
+}
+
+function prettyConfirm(title, text, callback) {
+    swal({
+        title: title,
+        text: text,
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55"
+    }, callback);
+}
+
+function prettyPrompt(title, text, inputValue, callback) {
+    swal({
+        title: title,
+        text: text,
+        type: 'input',
+        showCancelButton: true,
+        inputValue: inputValue
+    }, callback);
+}
